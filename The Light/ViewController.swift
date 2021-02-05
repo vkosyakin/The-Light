0aba70cda8d4141020bc24ae27fad105a53143e8
Valeriy Kosyakin <valeradzu@mail.ru>
2021-02-05 14:08:24 +0300

    Switched to touches began

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -8,26 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     var isLightOn = true
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
+    
+    // MARK : - Methobs
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
-        }
-    
-    fileprivate func updateUI() {
-       
-        view.backgroundColor = isLightOn ? .white : .black
     }
     
-    @IBAction func buttonPressed() {
+    fileprivate func updateUI() {
+        
+        view.backgroundColor = isLightOn ? .white : .black
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         isLightOn .toggle()
         updateUI()
         
     }
 }
-
+ 
